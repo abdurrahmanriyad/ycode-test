163d1454edf82d9c9aac38df485e1ca1141dc754
Abdur Rahman <me.riyad@outlook.com>
2021-02-24 20:46:20 +0600

    Add api airtable api key to config

---
+++ config/airtable.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Airtable api key
+    |--------------------------------------------------------------------------
+    |
+    | Api key that used and required to make requests to airtable api endpoints
+    |
+    */
+
+    'api_key' => env('AIRTABLE_API_KEY', null)
+];
